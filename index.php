1281bbc4a60a25dbd25f713a8027a21728ccc8dd
Anshu <anshuaditya@outlook.com>
2019-03-07 09:52:05 +0530

    footer modified

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html <?php language_attributes() ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ) ?>">
+	<title><?php bloginfo( 'name' ) ?></title>
+	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ) ?>">
+	<link href="https://fonts.googleapis.com/css?family=Roboto|Lobster|Source+Code+Pro|Bree+Serif|Markazi+Text|Scheherazade" rel="stylesheet">
+</head>
+<body>
+	
+	<header>
+		<h1 class="logo"><?php bloginfo( 'name' ); ?></h1>
+		<span class="description"><?php bloginfo( 'description' ); ?></span>
+	</header>
+
+	<div class="main">
+		<?php if( have_posts() ): ?>
+			<?php while ( have_posts() ): the_post() ?>
+	<h3 class="title"><?php the_title(); ?></h3>
+	<div class="meta">
+	Created By <?php the_author() ?> on <?php the_time( 'jS M Y' ) ?>
+	</div>
+	<div class="content"><?php the_content(); ?></div> 
+	<?php endwhile ?>
+	<?php else: ?>
+	<?php wpautop( "sorry, nothing found!" ) ?>
+	<?php endif ?>
+	<footer>
+		<p>&copy;- <?php the_date( 'Y' ); ?> <?php bloginfo( 'name' ) ?> </p>
+	</footer>
+	</div>
+
+</body>
+</html>
